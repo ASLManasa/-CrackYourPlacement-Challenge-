8ec4d422bc0c30743a3a910d70415b2155c015fa
ASLManasa <aslmanasa3@gmail.com>
2022-05-21 12:55:17 +0530

    arrays

---
+++ maxProfit1.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int profit =0;
+    int n = prices.length;
+    for(int i=1;i<n;i++){
+        if(prices[i]>prices[i-1]){
+            profit = profit+(prices[i] - prices[i-1]);
+        }
+    }
+    return profit;
+
+        
+    }
+}
